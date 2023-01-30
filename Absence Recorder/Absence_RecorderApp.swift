6895cbad8e6984d1e0206bcc2db77da2b8288ac2
whopes <hopes.w@etoncollege.org.uk>
2023-01-30 20:21:19 +0000

    Initial Commit

---
+++ Absence Recorder/Absence_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Absence_RecorderApp.swift
+//  Absence Recorder
+//
+//  Created by Hopes, William (AMM) on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Absence_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
